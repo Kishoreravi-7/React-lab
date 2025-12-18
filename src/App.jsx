545afedf5ccb807e8142ef997cdfc28fc4ecbb30
Kishoreravi-7 <k9990339@gmail.com>
2025-12-18 10:46:06 +0530

    crops

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Dashboard from './components/Dashboard'
 
 function App(){
+  let name="Sanji"
+  let occ="Luffy"
  return(
-    <>
-        <h1>SANJI</h1>
-        <h2>luffy</h2>
-    </>
+   <Dashboard name={name} occ={occ}/>
+    
  )
 }
 
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+import Card from "./Card"
+
+function Dashboard({name,occ}){
+    return(
+        
+         <Card fullname={name} occ={occ}/>
+         
+    )
+}
+export default Dashboard
